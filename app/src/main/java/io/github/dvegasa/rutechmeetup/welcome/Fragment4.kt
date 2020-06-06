af2743d26ec712051032b4a9ca892332202c06ed
Ed Khalturin <DracVeg@gmail.com>
2020-06-07 00:29:38 +0300

    Можно сделать фото

--- app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/Fragment4.kt
+++ app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/Fragment4.kt
@@ -1,13 +1,23 @@
 package io.github.dvegasa.rutechmeetup.welcome
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
+import android.provider.MediaStore
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 import io.github.dvegasa.rutechmeetup.R
+import kotlinx.android.synthetic.main.fragment_2.view.*
+import kotlinx.android.synthetic.main.fragment_4.*
 import kotlinx.android.synthetic.main.fragment_4.view.*
+import kotlinx.android.synthetic.main.fragment_4.view.ivAddPhoto
 
 /**
  * A simple [Fragment] subclass.
@@ -20,11 +30,33 @@ class Fragment4 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_4, container, false)
+
+        val span = SpannableString("Загрузи свою\nфотографию")
+        span.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FFCC00")),
+            7, 23,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        v.tvUploadPhoto.text = span
+
         v.btn4.setOnClickListener {
             (activity as WelcomeActivity).endRegistration()
         }
+        v.ivAddPhoto.setOnClickListener {
+            (activity as WelcomeActivity).makePhoto()
+        }
         return v
     }
+
+    override fun onResume() {
+        super.onResume()
+        if ((activity as WelcomeActivity).photo != null) {
+            ivAddPhoto.setImageBitmap((activity as WelcomeActivity).photo)
+
+        }
+    }
+
+
     companion object {
         @JvmStatic
         fun newInstance() = Fragment4()
--- app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/WelcomeActivity.kt
+++ app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/WelcomeActivity.kt
@@ -1,7 +1,11 @@
 package io.github.dvegasa.rutechmeetup.welcome
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import io.github.dvegasa.rutechmeetup.R
 import kotlinx.android.synthetic.main.activity_welcome.*
 
@@ -17,6 +21,7 @@ class WelcomeActivity : AppCompatActivity() {
 
     var name: String = ""
     var job: String = ""
+    var photo: Bitmap? = null
 
     private val nsvpAdapter = NsvpAdapter(frags, supportFragmentManager)
 
@@ -24,6 +29,14 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
         initNsvp()
+
+        btnConfirm.setOnClickListener {
+            endRegistration()
+        }
+
+        btnPhotoAgain.setOnClickListener {
+            makePhoto()
+        }
     }
 
     fun endRegistration() {
@@ -38,4 +51,27 @@ class WelcomeActivity : AppCompatActivity() {
         nsvp.adapter = nsvpAdapter
         nsvp.currentItem = 0
     }
+
+    private fun showPreview() {
+        nsvp.visibility = View.GONE
+        ivPreview.setImageBitmap(photo)
+    }
+
+    fun makePhoto() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, 42)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 42 && resultCode == RESULT_OK) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            photo = imageBitmap
+            showPreview()
+        }
+    }
+
 }
