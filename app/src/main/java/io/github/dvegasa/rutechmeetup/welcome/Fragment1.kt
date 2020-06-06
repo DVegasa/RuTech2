b159763b48b8077b6d43923964d58d7cbe801f58
Ed Khalturin <DracVeg@gmail.com>
2020-06-06 23:49:10 +0300

    Цветность

--- app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/Fragment1.kt
+++ app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/Fragment1.kt
@@ -1,6 +1,10 @@
 package io.github.dvegasa.rutechmeetup.welcome
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +25,15 @@ class Fragment1 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_1, container, false)
+
+        val span = SpannableString("Как тебя\nзовут?")
+        span.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FFCC00")),
+            9, 14,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        v.tvWhatName.text = span
+
         v.btn1.setOnClickListener {
             (activity as WelcomeActivity).name = etName.text.toString()
             (activity as WelcomeActivity).nextPage()
--- app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/Fragment2.kt
+++ app/src/main/java/io/github/dvegasa/rutechmeetup/welcome/Fragment2.kt
@@ -1,6 +1,10 @@
 package io.github.dvegasa.rutechmeetup.welcome
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +12,7 @@ import android.view.ViewGroup
 
 import io.github.dvegasa.rutechmeetup.R
 import kotlinx.android.synthetic.main.fragment_1.*
+import kotlinx.android.synthetic.main.fragment_1.view.*
 import kotlinx.android.synthetic.main.fragment_2.*
 import kotlinx.android.synthetic.main.fragment_2.view.*
 import kotlinx.android.synthetic.main.fragment_2.view.etJob
@@ -23,6 +28,15 @@ class Fragment2 : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val v = inflater.inflate(R.layout.fragment_2, container, false)
+
+        val span = SpannableString("Кем ты\nработаешь?")
+        span.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FFCC00")),
+            0, 4,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        v.tvWhatJob.text = span
+
         v.btn2.setOnClickListener {
             (activity as WelcomeActivity).job = etJob.text.toString()
             (activity as WelcomeActivity).nextPage()
